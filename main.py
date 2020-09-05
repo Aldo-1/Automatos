2bcd5cd6ad8a8317ae97008163be1f25c0a41aff
Aldo-1 <aldo.wanderley@souunit.com.br>
2020-09-05 19:03:48 -0300

    criando a leitura do dfa

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from modules.DFA import AutomatoFinitiDeterministico
+from modules.ler import *
 
+def init(automato, estadoInicial, estadoFinal, dfa):
+  while(True):
+    escolha = input('1) Ler arquivo de DFA e testar'+'\n'+ '0) Finalizar ' + '\n')
+    if(escolha == '1'):
+      while(True):
+        palavra = input('Digite a palavra para testar:(-1 sai do programa) = ')
+        if(palavra == '-1'):
+          break
+        else:
+          print(dfa.checkIsAccepted(automato, estadoInicial, estadoFinal , palavra))
+    else:
+      break
 
+estadoFinal = lerEstadoFinal('dfa.txt')
+estadoInicial = lerEstadoInicial('dfa.txt')
+automato = lerAutomatoDFA('dfa.txt')
 
-automato = AutomatoFinitiDeterministico()
+dfa = AutomatoFinitiDeterministico()
 
-automato.initProgram()
-
+init(automato,estadoInicial, estadoFinal, dfa)
 
---
+++ modules/ler.py
@@ -0,0 +1,70 @@
+def tirarEspacoVazio(lista):
+  for i in range(0,len(lista)):
+    lista[i] = lista[i].strip()
+    
+  return lista
+
+def lerAlfabeto(txt):
+  f = open(txt, 'r')
+  for line in f:
+    if('alfabeto' in line):
+      index = line.index('=')
+      alfabeto = line[index+1:].split(',')
+      alfabeto = tirarEspacoVazio(alfabeto)
+      break
+  f.close()
+  return alfabeto
+
+def lerEstadoInicial(txt):
+  f = open(txt, 'r')
+  for line in f:
+    if('estadoInicial' in line):
+      index = line.index('=')
+      estadoInicial = line[index+1:].strip()
+  f.close()
+  return estadoInicial
+
+def lerEstadoFinal(txt):
+  f = open(txt, 'r')
+  for line in f:
+    if('estadoFinal' in line):
+      index = line.index('=')
+      estadoFinal = set(line[index+1:].strip().split(','))
+  f.close()
+  
+  return estadoFinal
+
+def lerAutomatoDFA(txt):
+  automato = {}
+  dicionarioLetra = {}
+  f = open(txt, 'r')
+  for line in f:
+    alfabeto = lerAlfabeto(txt)
+    if(line.find('=') == -1 and line != '\n'):
+      indexDoisPontos = line.index(':')
+      estado = line[:indexDoisPontos].strip()
+      transicoes = line[indexDoisPontos+1:].split(',')
+      transicoes = tirarEspacoVazio(transicoes)
+   
+      for transicao in transicoes:
+        indexDoisPontosTransicao = transicao.index(':')
+        paraOndeVai = transicao[indexDoisPontosTransicao + 1:].strip()
+       
+        letra = transicao[:indexDoisPontos - 1].strip()
+        if(letra in alfabeto):
+          
+          automato[estado] = {}
+          dicionarioLetra[letra] = paraOndeVai
+        else:
+          print('ocorreu algum erro na execucao')
+          break
+      automato[estado] = dicionarioLetra
+      dicionarioLetra = {}
+ 
+        
+  f.close()
+  return automato
+
+
+
+
